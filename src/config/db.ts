40c73f6985131dcabe0b7c96c8078ae50f76c567
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2024-07-20 13:05:35 -0600

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,25 +1,30 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
+
 //setting up dotenv to read .env files. without any parameter means it will read from root project diretory
 dotenv.config();
 
-const connectionString = process.env.CONNECTION_STRING
+const connectionString = `${process.env.CONNECTION_STRING}${process.env.DB_NAME}`
 
 const connectToMongoDB = async()=>{
+    let connectionInstance;
     try{
         if(connectionString !==null && connectionString!==undefined){
-            await mongoose.connect(connectionString,{
+            connectionInstance = await mongoose.connect(connectionString,{
                 autoIndex:true
             })
             console.info('Connected to mongodb atlas')
         }
         else{
             console.error('Cannot connect to mongodb connection string empty')
+            process.exit(1);
         }
         
     }catch(error){
+        console.error('Could not connect to Database, exception happened')
         console.error(error)
+        process.exit(1);
     }
 }
 export {connectToMongoDB}
